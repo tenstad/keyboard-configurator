522bf04fae3bced0065bdebf98303ad436dc02f0
Amund Tenstad <github@amund.io>
2024-11-16 11:08:44 +0100

    feat: json input field

---
+++ src/lib/JsonInput.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let { data = $bindable() } = $props<{
+		data: object | undefined;
+	}>();
+
+	let value: string = $state('');
+	let error: any = $state(undefined);
+
+	$effect(() => {
+		try {
+			data = JSON.parse(value);
+			error = undefined;
+		} catch (e: any) {
+			data = undefined;
+			error = e;
+		}
+	});
+</script>
+
+<div>
+	<textarea bind:value rows="10"></textarea>
+	<p>{error}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
+<script lang="ts">
+    import JsonInput from '$lib/JsonInput.svelte'
+
+    let data: object | undefined = $state(undefined);
+</script>
+
+
 <h1>keyboard-configurator</h1>
+
+<JsonInput bind:data />
