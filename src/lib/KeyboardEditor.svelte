c0e66fa2d703cc6f24a8d1d1a7c84ae097f44e7d
Amund Tenstad <github@amund.io>
2024-11-16 17:59:54 +0100

    feat: drag swap

--- src/lib/KeyboardEditor.svelte
+++ src/lib/KeyboardEditor.svelte
@@ -1,18 +1,41 @@
 <script lang="ts">
 	let size = 50;
-	let gap = 4;
 
 	let { keyboard = $bindable(), keymap = $bindable() } = $props<{
 		keyboard: Keyboard;
 		keymap: Keymap;
 	}>();
 
+	let activeLayer: number = $state(0);
 	let activeLayout: string = $state(
 		Object.keys(keyboard.layouts).includes(keymap.layout)
 			? keymap.layout
 			: Object.keys(keyboard.layouts)[0]
 	);
-	let activeLayer: number = $state(0);
+
+	let dragged: number | undefined = $state(undefined);
+	let draggedOver: number | undefined = $state(undefined);
+
+	function handleDragStart(e: any, i: number) {
+		dragged = i;
+	}
+	function handleDragEnd(e: any, i: number) {
+		dragged = undefined;
+	}
+	function handleDragOver(e: any, i: number) {
+		draggedOver = i;
+	}
+	function handleDragExit(e: any, i: number) {
+		draggedOver = undefined;
+	}
+	function handleDrop(e: any, i: number) {
+		if (dragged !== undefined) {
+			let key = keymap.layers[activeLayer][dragged];
+			keymap.layers[activeLayer][dragged] = keymap.layers[activeLayer][i];
+			keymap.layers[activeLayer][i] = key;
+		}
+		draggedOver = undefined;
+	}
 </script>
 
 <div>
@@ -38,19 +61,35 @@
 	<div class="relative">
 		{#each keyboard.layouts[activeLayout].layout as key, i}
 			<div
-				class="absolute rounded-lg"
+				class="
+		{i === draggedOver ? `border-blue-200` : 'border-white'} {i === dragged
+					? 'bg-gray-200'
+					: ''} absolute border-2"
+				role="cell"
+				tabindex={i}
+				ondragover={(e) => {
+					e.preventDefault();
+					handleDragOver(e, i);
+				}}
+				ondragexit={(e) => handleDragExit(e, i)}
+				ondrop={(e) => handleDrop(e, i)}
 				style:left={`${size * key.x}px`}
 				style:top={`${size * key.y}px`}
 				style:width={`${size}px`}
-				style:height={`${size - gap}px`}
+				style:height={`${size}px`}
+				style:border-radius="10px"
 			>
-				<div
-					class="absolute flex h-full w-full items-center justify-center rounded-lg border border-gray-200 bg-gray-100 text-xs font-semibold text-gray-800"
-					style:margin={`${gap / 2}px`}
-					style:width={`${size - gap}px`}
-					style:height={`${size - gap}px`}
-				>
-					<span>{keymap.layers[activeLayer][i]}</span>
+				<div style:opacity={i == dragged ? 0 : 1}>
+					<div
+						class="absolute flex h-full w-full items-center justify-center rounded-lg border border-gray-200 bg-gray-100 text-xs font-semibold text-gray-800"
+						draggable="true"
+						role="button"
+						tabindex={i}
+						ondragstart={(e) => handleDragStart(e, i)}
+						ondragend={(e) => handleDragEnd(e, i)}
+					>
+						<span>{keymap.layers[activeLayer][i]}</span>
+					</div>
 				</div>
 			</div>
 		{/each}
