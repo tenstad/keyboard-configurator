503e1b6bd0aa2192b8e14d59137f34d83a699731
Amund Tenstad <github@amund.io>
2024-11-18 12:25:00 +0100

    style: darkmode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import DarkModeToggle from '$lib/DarkModeToggle.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
+
+<DarkModeToggle />
 
 {@render children()}
