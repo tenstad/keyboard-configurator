ac2ce7155285bbee86d67d4a2348ea7f8c2c6d9a
Amund Tenstad <github@amund.io>
2024-11-21 13:53:09 +0100

    feat: drag between layers

--- src/lib/TabButton.svelte
+++ src/lib/TabButton.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	let { onclick, active } = $props<{
+	let { onclick, active, ondragover } = $props<{
 		onclick: any;
+		ondragover: any;
 		active: boolean;
 	}>();
 </script>
@@ -8,5 +9,8 @@
 <button
 	class={(active ? 'bg-amber-300 dark:bg-amber-600' : 'bg-white dark:bg-neutral-700') +
 		' my-1 rounded py-1 pl-4 text-left hover:bg-amber-300 dark:text-white dark:hover:bg-amber-600'}
-	{onclick}><slot /></button
+	{onclick}
+	{ondragover}
 >
+	<slot />
+</button>
